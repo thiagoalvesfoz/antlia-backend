09da8348719e9082c5853c580abf08f0e1bbd1e6
Thiago Alves <thiago_marketingdigital@hotmail.com>
2023-10-22 05:14:03 -0300

    ref: remove path

--- src/account-manager/controller/user.controller.ts
+++ src/account-manager/controller/user.controller.ts
@@ -1,9 +1,8 @@
-import { Controller, Post, Body, Get, Param, Request } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { UsersService } from '../service/users.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { Roles } from 'src/auth/decorator/role.decorator';
 import { Role } from '../entity/role.entity';
-import { UserDto } from '../dto/user-response.dto';
 
 @Controller('users')
 export class UsersController {
@@ -25,11 +24,5 @@ export class UsersController {
   @Roles(Role.ADMIN)
   findOne(@Param('user_id') user_id: string) {
     return this.usersService.findOne(user_id);
-  }
-
-  @Get('/me')
-  me(@Request() req) {
-    const user: UserDto = req.user;
-    return user;
   }
 }
